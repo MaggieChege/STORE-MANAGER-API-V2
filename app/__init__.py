db9b8a7a1f1399d78eb8154757bcb34b7ca6878a
MaggieChege <shikooh.maggie1@gmail.com>
2018-11-11 00:06:58 +0300

    [ch-#161862364]setup heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask,Blueprint
 from app.dbconn import Database_Connection
-from flask_jwt_extended import JWTManager,get_raw_jwt
+# from flask_jwt_extended import JWTManager,get_raw_jwt
 from instance.config import app_config 
 from app.api.v2.models.users_model import *
 db = Database_Connection()
@@ -14,8 +14,8 @@ def create_app(config_name):
     from app.api.v2 import blue as v2
     app.register_blueprint(v2)
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-    app.config['JWT_BLACKLIST_ENABLED'] = True
-    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+    # app.config['JWT_BLACKLIST_ENABLED'] = True
+    # app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
     db.init_app(app)
     jwt.init_app(app)
 
