cdfc6adc3ef6ba99d946c696f3acb6005ceaa7ac
MaggieChege <shikooh.maggie1@gmail.com>
2018-11-11 09:44:55 +0300

    made changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask,Blueprint
-# from flask_jwt_extended import JWTManager
-# from app.api.v2.models.users_model import *
+from flask_jwt_extended import  JWTManager, jwt_required
 from app.dbconn import Database_Connection
 from instance.config import app_config 
 
@@ -12,7 +11,7 @@ db = Database_Connection()
 def create_app(config_name):
     app=Flask(__name__,instance_relative_config=True)
     jwt = JWTManager(app)
-    app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+    app.config['JWT_SECRET_KEY'] = 'SECRET'
     # app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
