341ec60ea56a1c14d4045767d59f387c0ef7a32b
MaggieChege <shikooh.maggie1@gmail.com>
2018-11-13 17:46:20 +0300

    Added CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask,Blueprint
 from app.dbconn import create_tables
 from flask_jwt_extended import JWTManager,get_raw_jwt
 from instance.config import app_config 
+from flask_cors import CORS
 from app.api.v2.models.users_model import *
 jwt = JWTManager()
 
@@ -13,10 +14,29 @@ def create_app(config_name):
     from app.api.v2 import blue as v2
     app.register_blueprint(v2)
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-    app.config['JWT_BLACKLIST_ENABLED'] = True
-    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+    # app.config['JWT_BLACKLIST_ENABLED'] = True
+    # app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
     create_tables()
+    CORS(app)
     jwt.init_app(app)
+
+
+
+
+    @app.errorhandler(404)
+    def not_found(e):
+        # defining function
+        return make_response(jsonify({
+            "Message": "Route not found. Please check on the route"
+        }), 404)
+
+    @app.errorhandler(500)
+    def internal_error(e):
+        return make_response(jsonify({
+            "Message": "Internal server"
+        }), 500)
+    return app
+
 
 
    
